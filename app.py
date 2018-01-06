6fae6d8a93c614987e521a908918a0c6eea1ad8f
worganic <worganic@gmail.com>
2018-01-06 21:15:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,14 @@
 import time
 import sys
 
+# Flask app should start in global layout
+app = Flask(__name__)
+
 print ("Script python test.py 2 ....")
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
