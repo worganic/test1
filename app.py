d049c40f5551ddad9527749fe075fe0863987bb9
worganic <worganic@gmail.com>
2018-01-06 21:18:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ print ("Script python test.py 2 ....")
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
-
+    print ("Starting app on port ")
+    
     app.run(debug=True, port=port, host='0.0.0.0')
